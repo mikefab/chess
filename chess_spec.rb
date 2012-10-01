e88d394770731bba15e1a648e9bef338c783016e
Michael Fabrikant <mikefab@Mikes-MacBook-Air.local>
2012-10-01 16:30:20 -0400

    adding test for right pieces in right squares at start.

--- chess_spec.rb
+++ chess_spec.rb
@@ -12,6 +12,28 @@ describe 'game' do
     subject.board.squares.size.should == 64
   end
 
+  it 'should have the right pieces in the right spots at the start of the game' do
+    pieces = {
+                'r' => [[0, 0, 'b'], [0, 7, 'b'],[7, 0, 'w'], [7, 7, 'w']],
+                'b' => [[0, 1, 'b'], [0, 6, 'b'],[7, 1, 'w'], [7, 6, 'w']],
+                'k' => [[0, 2, 'b'], [0, 5, 'b'],[7, 2, 'w'], [7, 5, 'w']],                
+                'q' => [[0, 3, 'b'], [7, 3, 'w']],                
+                'K' => [[0, 4, 'b'], [7, 4, 'w']]                
+              }
+    pieces.each do |piece, coords|
+      coords.each do |coord|
+        square = subject.board.squares.select{|square| square.x == coord[1] and square.y == coord[0]}[0]
+        square.piece.name.should == piece
+        square.piece.color.should == coord[2]
+        
+      end
+    end
+    
+
+  end
+
+
+
   it 'should only accept arguments in the appropriate format like "g2 e2"' do
     subject.valid?("g2 e2").should  == true
     subject.valid?("g2e2").should   == false
@@ -19,7 +41,7 @@ describe 'game' do
   end
 
   it 'should allow movement of pawns' do
-    start, stop = Square.convert_coordinates("g2 e2")
+    start, stop = Square.convert_coordinates("g2 e2") #g2 e2 are the coordinates for a square with a pawn
     start, stop = Square.get_squares_by_coordinates(start, stop, subject.board)
     start.piece.name.should == 'P' #should be a pawn
     subject.occupiable?(start, stop, 'w').should == true
